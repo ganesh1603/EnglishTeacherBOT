02c8ed02c5c09c1aa4a6588bcb600b51cecbd2e2
GANESH KUMAR E <118152658+ganesh1603@users.noreply.github.com>
2023-11-08 23:18:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ from langchain import PromptTemplate
 # App title
 st.set_page_config(page_title="👧English Teacher BOT")
 
-
-
 # Replicate Credentials
 with st.sidebar:
     st.title('👧English Teacher BOT')
@@ -20,9 +18,8 @@ MODEL_ID = "GPT-4"
 
 llm = Clarifai(pat=PAT, user_id=USER_ID, app_id=APP_ID, model_id=MODEL_ID)
 
-
 # Store LLM generated responses
-if "messages" not in st.session_state.keys():
+if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Hi i'm samantha English Teacher. how can i assist you?"}]
 
 # Display or clear chat messages
@@ -34,25 +31,24 @@ def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Hi i'm samantha English Teacher. how can i assist you?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-
 # Function for generating LLaMA2 response
-def generate_response(pred):
+def generate_llama2_response(pred):
     string_dialogue = '''As an AI language model specializing in English language teaching, your task is to engage in an interactive English lesson with a student named ALEX, who is keen on improving his professional English skills for career advancement.
-    Your instructions are to simulate a supportive and engaging English lesson that focuses on enhancing Ganesh's professional English proficiency. The lesson should be filled with practical advice, constructive feedback, and corrections to any mistakes made by ALEX in his English usage.
+    Your instructions are to simulate a supportive and engaging English lesson that focuses on enhancing ALEX's professional English proficiency. The lesson should be filled with practical advice, constructive feedback, and corrections to any mistakes made by ALEX in his English usage.
     Incorporate elements of humor, patience, and motivation to make the lesson engaging and realistic. The lesson should ideally be about 500-700 words long, depending on ALEX's responses.
     Remember, this is an interactive session between ALEX and the AI. Do not write the entire conversation in one go. Instead, provide a response and then wait for Ganesh's reply before continuing.{pred}'''
    
     for dict_message in st.session_state.messages:
         if dict_message["role"] == "user":
-            template += "User: " + dict_message["content"] + "\n\n"
+            string_dialogue += "User: " + dict_message["content"] + "\n\n"
         else:
-            template += "Assistant: " + dict_message["content"] + "\n\n"
+            string_dialogue += "Assistant: " + dict_message["content"] + "\n\n"
 
-    prompt_template = PromptTemplate(template=string_dialogue, input_variables=["pred"])
+    prompt = PromptTemplate(template=string_dialogue, input_variables=["pred"])
 
-    llm_chain = LLMChain(prompt=prompt_template, llm=llm)
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
 
-    output =llm_chain.run(pred)
+    output = llm_chain.run(pred)
     
     return output
 
@@ -66,12 +62,9 @@ if prompt := st.chat_input():
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_response(prompt)
+            response = generate_llama2_response(prompt)
             placeholder = st.empty()
-            full_response = ''
-            for item in response:
-                full_response += item
-                placeholder.markdown(full_response)
+            full_response = ''.join(response)
             placeholder.markdown(full_response)
     message = {"role": "assistant", "content": full_response}
     st.session_state.messages.append(message)
